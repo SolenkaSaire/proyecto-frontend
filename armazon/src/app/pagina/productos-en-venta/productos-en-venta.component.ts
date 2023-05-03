e289847d26f74e453d11a725eb2608c3b3a72822
cristian <Cristianr.gonzalezi@uqvirtual.edu.co>
2023-05-03 17:18:34 -0500

    creacion de carpetas vistas

---
+++ armazon/src/app/pagina/productos-en-venta/productos-en-venta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-en-venta',
+  templateUrl: './productos-en-venta.component.html',
+  styleUrls: ['./productos-en-venta.component.css']
+})
+export class ProductosEnVentaComponent {
+
+}
